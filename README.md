f1297a2670c2bcf6c14856f383b765cf3d9b2a75
aepsantos <rufferto__@hotmail.com>
2023-08-18 15:53:53 -0300

    corrção de classes

--- README.md
+++ README.md
@@ -36,7 +36,8 @@ start index.html
 
 Arthur Emannuel P. dos Santos 
 <div> 
-  <a href="https://www.linkedin.com/in/arthuremannuel-desenvolvedor/" target="_blank"><img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank"></a> 
+  <a href="https://www.linkedin.com/in/arthuremannuel-desenvolvedor/" target="_blank">
+  <img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white"></a> 
 </div>
 
 
--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 window.onload = listValueButton;
 
-
 function listValueButton(){
     let botao = document.querySelectorAll("input");
     for (let i = 0; i < botao.length; i++){
@@ -19,17 +18,11 @@ function envioValorBotao(event){
 let submit = document.getElementById("btnSubmit");
 
 submit.onclick = function(){
-    let enviar = document.getElementById("teste");
+    let enviar = document.getElementById("cardAnswer");
     enviar.style.display = "block"; 
     document.getElementById("card").style.display = "none";
-    document.getElementById("card").style.transform= "rotateY(180deg)";
-    
 }
 
-
-// let teste = " texto ";
-// let textoConfirmacao = document.getElementById("notaAvaliacao");
-//     textoConfirmacao.innerHTML = `You selected ${teste}out of 5`;
 
 
 
